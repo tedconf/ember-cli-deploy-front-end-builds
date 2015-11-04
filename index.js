fa180cf17f4ba06f283ab81b8b607c14dca29282
Ryan T <ryanto@gmail.com>
2015-11-04 18:36:31 -0500

    First pass at writing an ember-cli-deploy plugin

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 /* jshint node: true */
 'use strict';
 
+var BasePlugin = require('ember-cli-deploy-plugin');
+var passwdUser = require('passwd-user');
+
+var FrontEndBuildsNotifier = require('./lib/front-end-builds-notifier');
+
+
 module.exports = {
-  name: 'ember-cli-deploy-front-end-builds'
+  name: 'ember-cli-deploy-front-end-builds',
+
+  createDeployPlugin: function(options) {
+    var homedir = passwdUser.sync(process.getuid()).homedir;
+
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name,
+
+      defaultConfig: {
+        privateKey: homedir + '/.ssh/id_rsa',
+        requestOptions: {}
+      },
+
+      requiredConfig: [
+        'app',
+        'endpoint'
+      ],
+
+      didUpload: function(context) {
+        var notifier = new FrontEndBuildsNotifier({
+          plugin: this,
+          context: context
+        });
+
+        return notifier.notify();
+      }
+    });
+
+    return new DeployPlugin();
+  }
 };
